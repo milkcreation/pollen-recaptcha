4c46ed5c9922683d8160ae5aac19de33c83f3428
mast3rm1lk <mast3rm1lk@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2020-12-12 12:38:03 +0000

    recaptcha v1.0.x-dev

---
+++ recaptcha/branches/1.0/resources/config/recaptcha.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @see https://www.google.com/recaptcha/admin/
+ */
+return [
+    /**
+     * @var string $sitekey Recaptcha v2 Site key
+     */
+    'sitekey'   => 'xxxxxxx',
+    /**
+     * @var string $secretkey Recaptcha v2 Secret key
+     */
+    'secretkey' => 'zzzzzzz'
+];
---
+++ recaptcha/branches/1.0/tests/RecaptchaTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Pollen\Recaptcha\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class RecaptchaTest extends TestCase
+{
+
+}
