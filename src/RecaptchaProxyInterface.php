777933617270520cb10756a540574f8c2cf94751
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-28 19:04:15 +0000

    recaptcha >> 1.1.x-dev

---
+++ src/RecaptchaProxyInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pollen\Recaptcha;
+
+interface RecaptchaProxyInterface
+{
+    /**
+     * Instance du gestionnaire reCaptcha.
+     *
+     * @return RecaptchaInterface
+     */
+    public function recaptcha(): RecaptchaInterface;
+
+    /**
+     * Définition du gestionnaire reCaptcha.
+     *
+     * @param RecaptchaInterface $recaptcha
+     *
+     * @return RecaptchaProxy|static
+     */
+    public function setRecaptcha(RecaptchaInterface $recaptcha): RecaptchaProxy;
+}
